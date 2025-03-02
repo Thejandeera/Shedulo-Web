da43e4bf087281df61066bc5c553a8b735848c4b
Thejandeera <thejan1010@gmail.com>
2025-03-02 09:11:34 +0530

    added properties

---
+++ shedulo/shedulo/src/main/java/com/example/shedulo/SheduloApplication.java
@@ -0,0 +1,13 @@
+package com.example.shedulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheduloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheduloApplication.class, args);
+	}
+
+}
---
+++ shedulo/shedulo/src/test/java/com/example/shedulo/SheduloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shedulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheduloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
